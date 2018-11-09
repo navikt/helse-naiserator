3ab5bdfd68b6d01c8ba2f46f33c14314bbd96d79
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-09 12:10:52 +0100

    Try to fix repo name replacement

--- init.groovy
+++ init.groovy
@@ -1,10 +1,11 @@
 def repos = ["helse-sykepengebehandling", "helse-sykepengesoknadfilter", "helse-soknadsvalidator"]
 
 repos.each {
-    pipelineJob("${it}") {
+    REPO_NAME="${it}"
+    pipelineJob("${REPO_NAME}") {
         definition {
             cps {
-                scriptFile = readFileFromWorkspace('deploy-pipeline.groovy').replace('${REPO_NAME}', "${it}")
+                scriptFile = readFileFromWorkspace('deploy-pipeline.groovy').replace('${REPO_NAME}', "${REPO_NAME}")
                 script(scriptFile)
             }
         }
