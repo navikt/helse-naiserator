53c34a7b647bf6e4c3c425a702abb1b911abd99b
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-09 12:02:36 +0100

    Dont do string interpolation

--- init.groovy
+++ init.groovy
@@ -4,7 +4,7 @@ repos.each {
     pipelineJob("${it}") {
         definition {
             cps {
-                scriptFile = readFileFromWorkspace('deploy-pipeline.groovy').replace("${REPO_NAME}", "${it}")
+                scriptFile = readFileFromWorkspace('deploy-pipeline.groovy').replace('${REPO_NAME}', "${it}")
                 script(scriptFile)
             }
         }
